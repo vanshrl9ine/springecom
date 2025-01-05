d8e2e3fc22567c9b92e30a32473559fbc9040492
vanshrl9ine <guptavansh67@gmail.com>
2025-01-05 16:02:53 +0530

    APIresponse

--- src/main/java/com/ecommerce/ecom/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/ecom/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecom.exceptions;
 
+import com.ecommerce.ecom.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -28,13 +29,16 @@ public class MyGlobalExceptionHandler {
         return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
-       String message =e.getMessage();
-       return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e){
+        String message =e.getMessage();
+       APIResponse apiResponse = new APIResponse(message,false);
+       return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException e){
+    public ResponseEntity<APIResponse> myAPIException(APIException e){
         String message =e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+
+        APIResponse apiResponse = new APIResponse(message,false);
+        return new ResponseEntity<APIResponse>(apiResponse,HttpStatus.BAD_REQUEST);
     }
 }
