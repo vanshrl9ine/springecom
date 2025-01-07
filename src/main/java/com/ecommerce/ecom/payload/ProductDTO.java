45379af24877591c2ed6360dc36721d7056488fa
vanshrl9ine <guptavansh67@gmail.com>
2025-01-07 23:22:18 +0530

    product dto and response

---
+++ src/main/java/com/ecommerce/ecom/payload/ProductDTO.java
@@ -0,0 +1,19 @@
+package com.ecommerce.ecom.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private Double price;
+    private Double discount;
+    private double specialPrice;
+
+}
