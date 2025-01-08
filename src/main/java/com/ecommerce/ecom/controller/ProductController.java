86d4cda90986541387aae8c2099ead98458af826
vanshrl9ine <guptavansh67@gmail.com>
2025-01-08 12:12:12 +0530

    Search product by Category

--- src/main/java/com/ecommerce/ecom/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecom/controller/ProductController.java
@@ -28,4 +28,9 @@ public class ProductController {
         return new ResponseEntity<ProductResponse>(productResponse,HttpStatus.OK);
 
     }
+    @GetMapping("public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId) {
+         ProductResponse productResponse=productService.searchByCategory(categoryId);
+         return new ResponseEntity<ProductResponse>(productResponse,HttpStatus.OK);
+    }
 }
--- src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/ecom/repositories/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.ecommerce.ecom.repositories;
 
+import com.ecommerce.ecom.model.Category;
 import com.ecommerce.ecom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findByCategoryOrderByPriceAsc(Category category);
 
 }
